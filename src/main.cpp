06d526257138cf0134682e9b37fbded7848114b2
foundseven <122551432+foundseven@users.noreply.github.com>
2023-03-05 21:27:31 -0500

    MOCKUP_FIRST_MILESTONE

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,270 @@ bool isGameRunning = true;
 
 SDL_Window* pWindow = nullptr; // assignin a pointer to mullptr means the address is 0
 SDL_Renderer* pRenderer = nullptr; // NULL is the address 0. =NULL is essentiall the same as NULLptr
+
+
 SDL_Texture* pMySprite = nullptr;
 SDL_Rect mySpriteSrc;
 SDL_Rect mySpriteDst;
+
+struct background
+{
+public:
+
+	SDL_Texture* pTexture;
+	SDL_Rect src;
+	SDL_Rect dst;
+
+
+	// We can also create Member Functions that we call on.
+
+
+	background()
+	{
+		pTexture = nullptr;
+		src = { 0, 0, 0, 0 };
+		dst = { 0, 0, 0 ,0 };
+
+	}
+
+
+	//a non-default constructor
+	background(SDL_Renderer* renderer, const char* imageFilePath)
+	{
+
+		pTexture = IMG_LoadTexture(renderer, imageFilePath);
+
+		if (pTexture == NULL)
+		{
+			std::cout << "Image Load Failed! " << SDL_GetError() << std::endl;
+		}
+		else
+		{
+			std::cout << "Image Load Succesful! " << std::endl;
+		}
+
+		if (SDL_QueryTexture(pTexture, NULL, NULL, &src.w, &src.h) != 0)
+		{
+			std::cout << "Query Texture Failed! " << SDL_GetError() << std::endl;
+		}
+		src.x = 0;
+		src.y = 0;
+
+		dst.x = 0;
+		dst.y = 0;
+		dst.w = src.w;
+		dst.h = src.h;
+	}
+
+
+	void draw(SDL_Renderer* renderer)
+	{
+		int result = SDL_RenderCopy(renderer, pTexture, &src, &dst);
+		if (result != 0)
+		{
+			std::cout << "Render Failed!" << SDL_GetError() << std::endl;
+		}
+	}
+
+};
+
+background renderBackground = background(); // call the default constructor
+
+
+
+struct userPlayer 
+{
+public:
+
+SDL_Texture* usership;
+SDL_Rect src;
+SDL_Rect dst;
+
+
+// We can also create Member Functions that we call on.
+
+
+	userPlayer()
+	{
+		std::cout << "Defualt Constructor" << std::endl;
+		usership = nullptr;
+		src = { 0, 0, 0, 0 };
+		dst = { 0, 0, 0 ,0 };
+
+	}
+
+
+	//a non-default constructor
+	userPlayer(SDL_Renderer* renderer, const char* imageFilePath)
+	{
+		std::cout << "user player Constructor" << std::endl;
+		usership = IMG_LoadTexture(renderer, imageFilePath);
+
+		if (usership == NULL)
+		{
+			std::cout << "Image Load Failed! " << SDL_GetError() << std::endl;
+		}
+		else
+		{
+			std::cout << "Image Load Succesful! " << std::endl;
+		}
+
+		if ( SDL_QueryTexture(usership, NULL, NULL, &src.w, &src.h) != 0)
+		{
+			std::cout << "Query Texture Failed! " << SDL_GetError() << std::endl;
+		}
+		src.x = 0;
+		src.y = 0;
+
+		dst.x = 0;
+		dst.y = 0;
+		dst.w = src.w;
+		dst.h = src.h;
+	}
+
+
+void draw(SDL_Renderer* renderer)
+{
+	int result = SDL_RenderCopy(renderer, usership, &src, &dst);
+	if (result != 0)
+	{
+		std::cout << "Render Failed!" << SDL_GetError() << std::endl;
+	}
+}
+
+};
+
+userPlayer renderUserPlayer = userPlayer(); // call the default constructor
+userPlayer enemy1;
+
+struct obsticle
+{
+public:
+
+	SDL_Texture* object;
+	SDL_Rect src;
+	SDL_Rect dst;
+
+
+	// We can also create Member Functions that we call on.
+
+
+	obsticle()
+	{
+		std::cout << "Defualt Constructor" << std::endl;
+		object = nullptr;
+		src = { 0, 0, 0, 0 };
+		dst = { 0, 0, 0 ,0 };
+
+	}
+
+
+	//a non-default constructor
+	obsticle(SDL_Renderer* renderer, const char* imageFilePath)
+	{
+		std::cout << "user player Constructor" << std::endl;
+		object = IMG_LoadTexture(renderer, imageFilePath);
+
+		if (object == NULL)
+		{
+			std::cout << "Image Load Failed! " << SDL_GetError() << std::endl;
+		}
+		else
+		{
+			std::cout << "Image Load Succesful! " << std::endl;
+		}
+
+		if (SDL_QueryTexture(object, NULL, NULL, &src.w, &src.h) != 0)
+		{
+			std::cout << "Query Texture Failed! " << SDL_GetError() << std::endl;
+		}
+		src.x = 0;
+		src.y = 0;
+
+		dst.x = 0;
+		dst.y = 0;
+		dst.w = src.w;
+		dst.h = src.h;
+	}
+
+
+	void draw(SDL_Renderer* renderer)
+	{
+		int result = SDL_RenderCopy(renderer, object, &src, &dst);
+		if (result != 0)
+		{
+			std::cout << "Render Failed!" << SDL_GetError() << std::endl;
+		}
+	}
+
+};
+
+obsticle renderObject = obsticle();
+obsticle rock1;
+obsticle lighthouse;
+
+struct bullet
+{
+public:
+
+	SDL_Texture* ammo;
+	SDL_Rect src;
+	SDL_Rect dst;
+
+
+	// We can also create Member Functions that we call on.
+
+
+	bullet()
+	{
+		ammo = nullptr;
+		src = { 0, 0, 0, 0 };
+		dst = { 0, 0, 0 ,0 };
+
+	}
+
+
+	//a non-default constructor
+	bullet(SDL_Renderer* renderer, const char* imageFilePath)
+	{
+
+		ammo = IMG_LoadTexture(renderer, imageFilePath);
+
+		if (ammo == NULL)
+		{
+			std::cout << "Image Load Failed! " << SDL_GetError() << std::endl;
+		}
+		else
+		{
+			std::cout << "Image Load Succesful! " << std::endl;
+		}
+
+		if (SDL_QueryTexture(ammo, NULL, NULL, &src.w, &src.h) != 0)
+		{
+			std::cout << "Query Texture Failed! " << SDL_GetError() << std::endl;
+		}
+		src.x = 0;
+		src.y = 0;
+
+		dst.x = 0;
+		dst.y = 0;
+		dst.w = src.w;
+		dst.h = src.h;
+	}
+
+
+	void draw(SDL_Renderer* renderer)
+	{
+		int result = SDL_RenderCopy(renderer, ammo, &src, &dst);
+		if (result != 0)
+		{
+			std::cout << "Render Failed!" << SDL_GetError() << std::endl;
+		}
+	}
+
+};
+
+bullet renderAmmo = bullet(); // call the default constructor
 
 
 //init is short for initialize, we are setting p the game window, start SDL feature, ect
@@ -65,36 +326,84 @@ bool Init()
 }
 
 
+
 void load()
 {
-	//load texture...
+	//loading all textures...
 	//SDL_Texture * IMG_LoadTexture(SDL_Renderer *renderer, const char *file)
-
-	pMySprite = IMG_LoadTexture(pRenderer, "../Assets/textures/enterprise.png");
-
-	if (pMySprite == NULL)
-	{
-		std::cout << "Image Load Failed! " << SDL_GetError() << std::endl;
-	}
-	else
-	{
-		std::cout << "Image Load Succesful! " << std::endl;
-	}
+	renderBackground = background(pRenderer, "../Assets/textures/Still_water_image.png");
 
 
-	mySpriteSrc.x = 0;
-	mySpriteSrc.y = 0;
-	mySpriteSrc.w = 643;
-	mySpriteSrc.h = 296;
+	int backgroundWidth = renderBackground.src.w ;
+	int backgroundHeight = renderBackground.src.h ;
 
-	int shipWidth = mySpriteSrc.w / 4;
-	int shipHeight = mySpriteSrc.h / 4;
+	renderBackground.dst.w = backgroundWidth;
+	renderBackground.dst.h = backgroundHeight;
 
-	mySpriteDst.w = shipWidth;
-	mySpriteDst.h = shipHeight;
-	mySpriteDst.x = (WINDOW_WIDTH / 8); //start with left eighth
-	mySpriteDst.y = (WINDOW_HEIGHT / 2) - shipHeight/2; // in middle
+	/////////////////////////////////////////////////
+
+	renderUserPlayer = userPlayer(pRenderer, "../Assets/textures/usership.png");
+
+
+	int userWidth = renderUserPlayer.src.w / 10;
+	int userHeight = renderUserPlayer.src.h / 10;
+
+	renderUserPlayer.dst.w =  userWidth;
+	renderUserPlayer.dst.h = userHeight;
+	renderUserPlayer.dst.x = (WINDOW_WIDTH / 8); //start with left eighth
+	renderUserPlayer.dst.y = (WINDOW_HEIGHT / 2) - userHeight/2; // in middle
+
+	enemy1 = userPlayer(pRenderer, "../Assets/textures/enemy1.png");
+	enemy1.dst.x = 1200;
+	enemy1.dst.y = 320;
+
+	/////////////////////////////////////////////////
+
+	renderObject = obsticle(pRenderer, "../Assets/textures/kelp.png");
+
+
+	int objectWidth = renderObject.src.w /2;
+	int objectHeight = renderObject.src.h /2;
+
+	renderObject.dst.w = objectWidth;
+	renderObject.dst.h = objectHeight;
+	renderObject.dst.x = (WINDOW_WIDTH /  2); //start with left eighth
+	renderObject.dst.y = (WINDOW_HEIGHT / 7) ; // and up
+
+
+	rock1 = obsticle(pRenderer, "../Assets/textures/rock1.png");
+	rock1.dst.x = 700;
+	rock1.dst.y = 200;
+
+	lighthouse = obsticle(pRenderer, "../Assets/textures/lighthouse.png");
+	
+	int lighthouseWidth = lighthouse.src.w / 4;
+	int lighthouseHeight = lighthouse.src.h / 4;
+
+	lighthouse.dst.w = lighthouseWidth;
+	lighthouse.dst.h = lighthouseHeight;
+	lighthouse.dst.x = 1000;
+	lighthouse.dst.y = 450;
+	
+
+	///////////////////////////////////////////////////
+
+	renderAmmo = bullet(pRenderer, "../Assets/textures/cannonball.png");
+
+
+	int ammoWidth = renderAmmo.src.w / 2;
+	int ammoHeight = renderAmmo.src.h / 2;
+
+	renderAmmo.dst.w = ammoWidth;
+	renderAmmo.dst.h = ammoHeight;
+	renderAmmo.dst.x = (WINDOW_WIDTH / 4); //start with left eighth
+	renderAmmo.dst.y = (WINDOW_HEIGHT / 2); // and up
+
 }
+
+
+
+
 
 void Input()
 {
@@ -104,7 +413,11 @@ void Input()
 
 void Update()
 {
-	mySpriteDst.x = mySpriteDst.x + 1;
+	//making the enemy move 
+	enemy1.dst.x = enemy1.dst.x - 1;
+
+	//making the ammo move 
+	renderAmmo.dst.x = renderAmmo.dst.x + 4;
 }
 
 void Draw()
@@ -117,6 +430,27 @@ void Draw()
 	{
 		std::cout << "Render Failed!" << SDL_GetError() << std::endl;
 	}
+
+	// calling on the draw function for background
+	renderBackground.draw(pRenderer);
+
+	// calling on user ship
+	renderUserPlayer.draw(pRenderer);
+
+	//drawing enemy
+	enemy1.draw(pRenderer);
+
+	//calling on first object
+	renderObject.draw(pRenderer);
+
+	//calling on rock1
+	rock1.draw(pRenderer);
+
+	//calling on cannonball
+	renderAmmo.draw(pRenderer);
+
+	// calling on lighthouse
+	lighthouse.draw(pRenderer);
 
 	//show the back buffer
 	SDL_RenderPresent(pRenderer);
